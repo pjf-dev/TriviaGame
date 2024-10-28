11c268bdba615ac30ba5d646abe758112e270dae
pjf-dev <28768673+pjf-dev@users.noreply.github.com>
2024-10-28 15:07:23 -0400

    Update TriviaQuestion to record class type

--- app/src/main/java/ung/csci3660/fall2024/triviagame/api/TriviaQuestion.java
+++ app/src/main/java/ung/csci3660/fall2024/triviagame/api/TriviaQuestion.java
@@ -1,47 +1,10 @@
 package ung.csci3660.fall2024.triviagame.api;
 
 // Fairly self-explanatory
-public class TriviaQuestion {
-
-    private final Type type;
-    private final Difficulty difficulty;
-    private final String question;
-    private final String categoryString;
-    private final String correctAnswer;
-    private final String[] incorrectAnswers;
-
-    public TriviaQuestion(Type type, Difficulty difficulty, String question, String categoryString, String correctAnswer, String[] incorrectAnswers) {
-        this.type = type;
-        this.difficulty = difficulty;
-        this.question = question;
-        this.categoryString = categoryString;
-        this.correctAnswer = correctAnswer;
-        this.incorrectAnswers = incorrectAnswers;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public Difficulty getDifficulty() {
-        return difficulty;
-    }
-
-    public String getQuestion() {
-        return question;
-    }
-
-    public String getCategoryString() {
-        return categoryString;
-    }
-
-    public String getCorrectAnswer() {
-        return correctAnswer;
-    }
-
-    public String[] getIncorrectAnswers() {
-        return incorrectAnswers;
-    }
+// Record class type added in Java 14
+public record TriviaQuestion(ung.csci3660.fall2024.triviagame.api.TriviaQuestion.Type type,
+                             ung.csci3660.fall2024.triviagame.api.TriviaQuestion.Difficulty difficulty, String question,
+                             String categoryString, String correctAnswer, String[] incorrectAnswers) {
 
     public enum Type {
         BOOLEAN,
