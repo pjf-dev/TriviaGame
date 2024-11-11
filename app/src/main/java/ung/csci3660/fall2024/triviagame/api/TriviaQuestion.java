f31f50f93108430aed2fecbb5d75f0d98f8c3105
pjf-dev <28768673+pjf-dev@users.noreply.github.com>
2024-11-11 11:29:52 -0500

    Implement Parcelable on TriviaQuestion

--- app/src/main/java/ung/csci3660/fall2024/triviagame/api/TriviaQuestion.java
+++ app/src/main/java/ung/csci3660/fall2024/triviagame/api/TriviaQuestion.java
@@ -1,10 +1,13 @@
 package ung.csci3660.fall2024.triviagame.api;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 // Fairly self-explanatory
 // Record class type added in Java 14
 public record TriviaQuestion(ung.csci3660.fall2024.triviagame.api.TriviaQuestion.Type type,
                              ung.csci3660.fall2024.triviagame.api.TriviaQuestion.Difficulty difficulty, String question,
-                             String categoryString, String correctAnswer, String[] incorrectAnswers) {
+                             String categoryString, String correctAnswer, String[] incorrectAnswers) implements Parcelable {
 
     public enum Type {
         BOOLEAN,
@@ -19,4 +22,39 @@ public record TriviaQuestion(ung.csci3660.fall2024.triviagame.api.TriviaQuestion
         ANY // Used for query only, never for a question
     }
 
+    protected TriviaQuestion(Parcel in) {
+        this(TriviaQuestion.Type.valueOf(in.readString()),
+                Difficulty.valueOf(in.readString()),
+                in.readString(),
+                in.readString(),
+                in.readString(),
+                in.createStringArray());
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(type.toString());
+        dest.writeString(difficulty.toString());
+        dest.writeString(question);
+        dest.writeString(categoryString);
+        dest.writeString(correctAnswer);
+        dest.writeStringArray(incorrectAnswers);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<TriviaQuestion> CREATOR = new Creator<>() {
+        @Override
+        public TriviaQuestion createFromParcel(Parcel in) {
+            return new TriviaQuestion(in);
+        }
+
+        @Override
+        public TriviaQuestion[] newArray(int size) {
+            return new TriviaQuestion[size];
+        }
+    };
 }
