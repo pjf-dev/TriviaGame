66700ebeb4ea92f06c542634c82e86a00550eb3e
danielcollis <daniel.collis13@gmail.com>
2024-11-13 17:34:27 -0500

    added splash screen with splash_logo.png as a logo (logo not showing up on splash screen, however)

--- app/src/main/java/ung/csci3660/fall2024/triviagame/MainActivity.java
+++ app/src/main/java/ung/csci3660/fall2024/triviagame/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_TriviaGame);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
